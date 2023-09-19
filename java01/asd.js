c7a784393bcf9f89ab444e026ee5a32b105a6201
Jaggerrrrr <1601217196@qq.com>
2023-09-19 22:58:45 +0800

    第一次试验

---
+++ java01/asd.js
@@ -0,0 +1,12 @@
+var asd = prompt('请您输入一个数字')
+switch (asd){
+    case'1':
+        alert('6666')
+        break;
+    case'2':
+        alert(666666)
+        break;
+    default:
+        alert('》？？？？《')
+
+}
---
+++ java01/src/com/learning01/test/JavaTest.java
@@ -0,0 +1,21 @@
+package com.learning01.test;
+
+import org.junit.Test;
+
+public class JavaTest {
+    @Test
+    public void test1(){
+        System.out.println(123);
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("abc");
+    }
+
+    @Test
+    public void test123(){
+        System.out.println("jagger");
+    }
+
+}
